f39c420203e8ebe446a199ae2d390104a5f274ba
kshashankrao <shashank.kadapanatham@gmail.com>
2020-09-10 03:34:28 +0200

    First version of the application.

--- src/config_reader.cpp
+++ src/config_reader.cpp
@@ -1,25 +1,53 @@
+/*
+Author: Shashank Rao
+*/
+
 #include "config_reader.h"
 #include <fstream>
 #include <cassert>
 #include <filesystem>
+#include <algorithm>
+#include <exception>
 
 void config_reader::read()
 {	
+	const std::string delimiter = "=";
 	bool is_file = std::filesystem::exists(m_path);
 	if ( is_file == true)
 	{
+		std::cout << "[INFO] Config file found" << std::endl;
 		std::ifstream inputFile(m_path);
 		
 		for (std::string line; getline(inputFile, line);)
 		{
-			std::cout << line << std::endl;
+			line.erase(std::remove_if(line.begin(), line.end(), isspace), line.end());
+			std::string key = line.substr(0, line.find(delimiter));
+			std::string value = line.substr(line.find(delimiter)+1, line.size());
+			data.insert(std::pair<std::string, std::string>(key, value));
 		}
+	}
+
+	else
+	{
+		std::cout << "[INFO] Config file not found" << std::endl;
 	}
 }
 
+std::string config_reader::get_value(std::string key)
+{
+	try
+	{
+		return data.at(key);
+	}
+
+	catch (std::exception& e)
+	{
+		std::cout << "[INFO] Error - Key not found: " << e.what() << std::endl;
+	}
+}
 int main()
 {
-	config_reader reader("D:\\DeepLearning\\config_reader\\data\\test_config.txt");
+	config_reader reader("path\\to\\config.txt");
 	reader.read();
-
+	std::cout << reader.get_value("name");
 }
--- src/config_reader.h
+++ src/config_reader.h
@@ -1,3 +1,7 @@
+/*
+Author: Shashank Rao
+*/
+
 #pragma once
 #include <iostream>
 #include <stdio.h>
@@ -10,12 +14,23 @@ class config_reader
 public:
 	config_reader(std::string path):m_path(path)
 	{}
-	
+
+	/* Read the config file. Store the key and values in the map.*/
 	void read();
+
+	/* Input: Key 
+	   Returns: Corresponding value*/
 	std::string get_value(std::string key);
+
+	/* Return: Map with key and value */
+	std::map<std::string, std::string> get_data()
+	{
+		return data;
+	}
+
 
 private:
 	const std::string m_path;
-	//std::map<std::string, std::string> data;
+	std::map<std::string, std::string> data;
 
 };
